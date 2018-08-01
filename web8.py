033dfedcf451d3df3d410edd074a4307827abe46
aicozone <413354463@qq.com>
2018-08-01 21:12:10 +0800

    修改

--- web8.py
+++ web8.py
@@ -111,7 +111,7 @@ https://robomongo.org/download
 注意，需要安装 pymongo 这个库
 pip3 install pymongo
 在你安装并开启 mongo 之后，就可以使用 pymongo 来连接使用 mongodb 了
-
+mongodb 使用的是 Javascript 语句
 mongo_demo 执行流程:
     1. 创建连接:
         # 连接 mongo 数据库, 主机是本机, 端口是默认的端口
